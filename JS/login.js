4863b065e388b80591cd4cfb754cf3d1eded7da6
Asmitapaudel <asmeetapdl@gmail.com>
2023-06-19 11:27:55 +0545

    change the image using  setInterval

--- JS/login.js
+++ JS/login.js
@@ -11,3 +11,19 @@ togglePassword.addEventListener("click", function (e) {
 });
 
 togglePassword.classList.add("fa-eye-slash");
+
+//　for image
+var image = document.getElementById("Imagecontainer");
+var images = [
+  "../assets/download (1).jpeg",
+  "../assets/download (2).jpeg",
+  "../assets/download.jpeg",
+]; // Array of image URLs
+var currentIndex = 0;
+
+function changeImage() {
+  image.src = images[currentIndex];
+  currentIndex = (currentIndex + 1) % images.length; // Update current index
+}
+
+setInterval(changeImage, 3000);
