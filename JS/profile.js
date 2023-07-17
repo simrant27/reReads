7eb6ed5f40d30c0d716ceed69c59da7ed20b8b54
Anmoleden <be2020se702@gces.edu.np>
2023-07-17 09:50:38 +0545

    create upload book form and add sell and actualprice

--- JS/profile.js
+++ JS/profile.js
@@ -46,3 +46,173 @@ function saveProfileChanges(event) {
   
   cancelEditForm();
 }
+
+function openUploadBookForm() {
+  var uploadWindow = window.open("", "_blank", "width=400,height=500");
+  uploadWindow.document.write(`
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Upload Book</title>
+      <style>
+        /* Styles for the form and other elements */
+
+        body {
+          font-family: "Arial", sans-serif;
+          background-color: #f8f8f8;
+        }
+
+        h1 {
+          font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+          font-size: 24px;
+          font-weight: bold;
+          margin-bottom: 20px;
+          color: #333;
+        }
+
+        form {
+          font-size: 16px;
+        }
+
+        .form-row {
+          display: flex;
+          align-items: center;
+          margin-bottom: 15px;
+        }
+
+        label {
+          font-weight: bold;
+          font-family: "Roboto", Arial, sans-serif;
+          flex-basis: 30%;
+          color: #333;
+        }
+
+        input[type="text"],
+        input[type="number"],
+        input[type="date"] {
+          padding: 8px;
+          font-family: "Courier New", monospace;
+          font-size: 14px;
+          border: 1px solid #ccc;
+          border-radius: 4px;
+          flex-basis: 70%;
+        }
+
+        .radio-group {
+          display: flex;
+          align-items: center;
+        }
+
+        .radio-group label {
+          margin-right: 10px;
+        }
+
+        .additional-form {
+          display: none;
+        }
+
+        button[type="submit"] {
+          padding: 10px 20px;
+          font-family: "Montserrat", Arial, sans-serif;
+          font-size: 16px;
+          background-color: #4CAF50;
+          color: white;
+          border: none;
+          cursor: pointer;
+          border-radius: 4px;
+        }
+      </style>
+    </head>
+    <body>
+      <h1>Upload Book</h1>
+      <form id="upload-form" onsubmit="submitBook(event)">
+        <div class="form-row">
+          <label for="book-name">Book Name:</label>
+          <input type="text" id="book-name" name="book-name" required>
+        </div>
+        <div class="form-row">
+          <label for="author-name">Author Name:</label>
+          <input type="text" id="author-name" name="author-name" required>
+        </div>
+        <div class="form-row">
+          <label for="genre">Genre:</label>
+          <input type="text" id="genre" name="genre" required>
+        </div>
+        <div class="form-row">
+          <label for="language">Language:</label>
+          <input type="text" id="language" name="language" required>
+        </div>
+        <div class="form-row">
+          <label for="published-date">Published Date:</label>
+          <input type="text" id="published-date" name="published-date" placeholder="mm/dd/yyyy" required>
+        </div>
+        <div class="form-row">
+          <label for="publisher">Publisher:</label>
+          <input type="text" id="publisher" name="publisher" required>
+        </div>
+        <div class="form-row">
+          <label for="actual-price">Actual Price:</label>
+          <input type="number" id="actual-price" name="actual-price" required>
+        </div>
+        <div class="form-row">
+          <label for="transaction-type">Transaction Type:</label>
+          <div class="radio-group">
+            <label for="donate">
+              <input type="radio" id="donate" name="transaction-type" value="donate" required>
+              Donate
+            </label>
+            <label for="sale">
+              <input type="radio" id="sale" name="transaction-type" value="sale" required onclick="toggleAdditionalForm(this)">
+              Sale
+            </label>
+          </div>
+        </div>
+        <div class="additional-form">
+          <div class="form-row">
+            <label for="selling-price">Selling Price:</label>
+            <input type="number" id="selling-price" name="selling-price" required>
+          </div>
+          <div class="form-row">
+            <label for="actual-price-sale">Actual Price:</label>
+            <input type="number" id="actual-price-sale" name="actual-price-sale" required>
+          </div>
+        </div>
+        <button type="submit">Upload</button>
+      </form>
+      <script>
+        function toggleAdditionalForm(radio) {
+          var additionalForm = document.querySelector('.additional-form');
+          additionalForm.style.display = radio.checked ? 'block' : 'none';
+        }
+
+        function submitBook(event) {
+          event.preventDefault();
+          // Handle the form submission and book upload logic here
+          // You can access the form data using the DOM API
+          var bookName = document.getElementById('book-name').value;
+          var authorName = document.getElementById('author-name').value;
+          var genre = document.getElementById('genre').value;
+          var language = document.getElementById('language').value;
+          var publishedDate = document.getElementById('published-date').value;
+          var publisher = document.getElementById('publisher').value;
+          var actualPrice = document.getElementById('actual-price').value;
+          var transactionType = document.querySelector('input[name="transaction-type"]:checked').value;
+          var sellingPrice = document.getElementById('selling-price').value;
+          var actualPriceSale = document.getElementById('actual-price-sale').value;
+
+          // Add your logic to handle the form submission
+        }
+      </script>
+    </body>
+    </html>
+`);
+
+
+
+
+  uploadWindow.document.close();
+}
+
+
