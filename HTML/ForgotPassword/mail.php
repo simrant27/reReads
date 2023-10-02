eff5f1b8407ffae708fb47f0e86516312d912cc4
Asmitapaudel <asmeetapdl@gmail.com>
2023-10-02 19:39:28 +0545

    add PHPMailer

--- HTML/ForgotPassword/mail.php
+++ HTML/ForgotPassword/mail.php
@@ -8,9 +8,9 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-  require '../../PHPMailer/src/Exception.php';
-  require '../../PHPMailer/src/PHPMailer.php';
-  require '../../PHPMailer/src/SMTP.php';
+  require '../../PHPMailer-master/src/Exception.php';
+  require '../../PHPMailer-master/src/PHPMailer.php';
+  require '../../PHPMailer-master/src/SMTP.php';
 
 $mail = new PHPMailer(true); // Enable exceptions
 
