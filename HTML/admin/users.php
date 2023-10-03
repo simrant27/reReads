d945813504200c6d9407b1e723fbf8f218110f66
Asmitapaudel <asmeetapdl@gmail.com>
2023-10-03 23:15:18 +0545

    some change in admin user ui

--- HTML/admin/users.php
+++ HTML/admin/users.php
@@ -7,7 +7,7 @@ function fetchUsers($conn)
     $users = array();
 
     // Query to fetch users
-   $query = "SELECT users.fullName AS user_name,
+    $query = "SELECT users.fullName AS user_name,
                  users.user_img AS image,
                  users.address AS Address,
                  users.email AS email,
@@ -17,12 +17,12 @@ function fetchUsers($conn)
           WHERE users.email != 'rereads3@gmail.com'
           ORDER BY users.user_id DESC";
 
-
     $result = $conn->query($query);
 
     if ($result) {
         $count = $result->num_rows; // Count the number of users
         while ($row = $result->fetch_assoc()) {
+            $row['image_data'] = base64_decode($row['image']); // Decode base64 image data
             $users[] = $row;
         }
     }
@@ -30,15 +30,25 @@ function fetchUsers($conn)
     return array($users, $count);
 }
 
+// Function to determine the MIME type of an image
+function getImageMimeType($imageData)
+{
+    $finfo = finfo_open(FILEINFO_MIME_TYPE);
+    $mimeType = finfo_buffer($finfo, $imageData);
+    finfo_close($finfo);
+    return $mimeType;
+}
+
 // Function to delete a user
 function deleteUser($conn, $userId)
 {
     // You should add proper error handling here, e.g., try-catch blocks.
-    
-    // Query to delete a user by user_id
-    $query = "DELETE FROM users WHERE user_id = $userId";
-    
-    if ($conn->query($query)) {
+
+    // Use prepared statements to prevent SQL injection
+    $stmt = $conn->prepare("DELETE FROM users WHERE user_id = ?");
+    $stmt->bind_param("i", $userId);
+
+    if ($stmt->execute()) {
         return true; // Deletion successful
     } else {
         return false; // Deletion failed
@@ -51,16 +61,17 @@ list($users, $count) = fetchUsers($conn);
 // Check if a delete request was made
 if (isset($_POST['delete_user_id'])) {
     $userIdToDelete = $_POST['delete_user_id'];
-    
+
     if (deleteUser($conn, $userIdToDelete)) {
         // Refresh the page or redirect to update the user list
-        header('Location:http://localhost/reReads/HTML/admin/users.php');
+        header('Location: http://localhost/reReads/HTML/admin/users.php');
         exit;
     } else {
         echo "Error deleting user.";
     }
 }
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -71,15 +82,26 @@ if (isset($_POST['delete_user_id'])) {
     <title>Users </title></head>
 <body   id="bg-img"
     style="background-image: url('../../assets/backgroundImage/download5.jpeg')">
-    <div class="header">
+    <nav class="navbar">
+      <?php
+include "../navbar/navbar.php";
+?>
+    </nav>
+    <div class="header_user">
             <h3 class="users-title">User List: No_of Users:
               <?php echo$count ?></h3>
             <ul>
-                <?php foreach ($users as $user): ?>
+                <?php foreach ($users as $user):?>
                     <li>
                       <div class="users-details">
-                            <img src="../../assets/profile_picture/<?php echo $user['image']; ?>" alt="<?php echo $user['user_name']; ?>">
-                            <div class="user-details">
+                         <!-- Determine the MIME type dynamically -->
+                        <?php
+                        $imageData = $user['image'];
+                        $mimeType = getImageMimeType($imageData);
+                        ?>
+                        <img src="data:<?php echo $mimeType; ?>;base64,<?php echo base64_encode($imageData); ?>" alt="<?php echo"image"; ?>">
+                         
+                            <div class="user-image">
                                 <span class="user-name"><?php echo $user['user_name']; ?></span>    
                             </div>
                         </div>
@@ -93,7 +115,8 @@ if (isset($_POST['delete_user_id'])) {
                             <button type="submit" class="delete-button">Delete</button>
                         </form>
                     </li>
-                <?php endforeach;?>
+
+                <?php  endforeach;?>
             </ul>
     </div>
         <script src="../../JS/Background/background.js"></script>
