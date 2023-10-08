c9c1278e61922b94454d9fe130faaee5ee8f22f9
Asmitapaudel <asmeetapdl@gmail.com>
2023-10-08 18:18:39 +0545

    book.php

--- HTML/admin/users.php
+++ HTML/admin/users.php
@@ -6,21 +6,18 @@ include_once "../../References/connection.php";
 function fetchUsers($conn)
 {
     $users = array();
-    // Query to fetch users
-    $query = "SELECT DISTINCT users.fullName AS user_name,
+    // Query to fetch users with associated books
+    $query = "SELECT
+                users.fullName AS user_name,
                 users.user_img AS image,
                 users.address AS Address,
                 users.email AS email,
                 users.phoneNo AS Number,
-                users.user_id AS user_id,
-                books.images AS image_book,
-                books.book_name AS book_name,
-                books.user_id AS user_book_id,
-                books.book_id AS bookid
+                users.user_id AS user_id
             FROM users
-            LEFT JOIN books ON users.user_id = books.user_id
             WHERE users.email != 'rereads3@gmail.com'
-            ORDER BY users.user_id DESC";
+            ORDER BY users.user_id DESC;
+    ";
 
     $result = $conn->query($query);
 
@@ -61,6 +58,7 @@ if (isset($_POST['delete_user_id'])) {
         exit;
     } else {
         echo "Error deleting user.";
+        header('Location: http://localhost/reReads/HTML/admin/users.php');
     }
 }
 ?>
@@ -71,14 +69,21 @@ if (isset($_POST['delete_user_id'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../CSS/admin/users.css">
+      <link rel="stylesheet" href="../../CSS/homepage/homepage.css" />
+
     <title>Users</title>
 </head>
 <body id="bg-img" style="background-image: url('../../assets/backgroundImage/download5.jpeg')">
-    <nav class="navbar">
-        <?php include "../navbar/navbar.php";?>
-    </nav>
+    <nav>
+      <div class="navbar lower-nav">
+        <ul class="lower-nav nav-list">
+          <li><a href="../homepage/homepage.php"> Home</a></li>
+          <li><a href="../AboutUs/aboutUs.php">About us</a></li>
+        </ul>
+      </div>
+  </nav>
     <div class="header_user">
-        <h3 class="users-title">User List: No_of Users: <?php echo $count ?></h3>
+        <h3 class="users-title">User List: No_of Users: <?php echo $count-1?></h3>
         <?php foreach ($users as $user): ?>
             <ul>
                 <!-- Inside the <li> for each user -->
@@ -89,45 +94,28 @@ if (isset($_POST['delete_user_id'])) {
                             <span class="user-name"><?php echo $user['user_name']; ?></span>
                         </div>
                     </div>
+                                       
                     <a href="#" class="user-link" onclick="toggleBookDetails(<?php echo $user['user_id']; ?>)">
+
                         <div class="user-details">
                             <div class="user-detail"><?php echo $user['email']; ?></div>
                             <div class="user-detail"><?php echo $user['Address']; ?></div>
                             <div class="user-detail"><?php echo $user['Number']; ?></div>
                         </div>
                     </a>
+
                     <form method="POST">
                         <input type="hidden" name="delete_user_id" value="<?php echo $user['user_id']; ?>">
                         <button type="submit" class="delete-button">Delete</button>
                     </form>
-                    </li>
-
+                </li>
             </ul>
-                    <?php
-if ($user['user_book_id'] == $user['user_id']): ?>
-                        <!-- Book details container -->
-                        <div class="book-details-container" id="book-details-<?php echo $user['user_id']; ?>">
-                            <ul>
-                                <li>
-                                    <div class="book-user">
-                                        <img src="../../assets/uploads/<?php echo $user['image_book']; ?>" alt="<?php echo $user['user_name']; ?>">
-                                    </div>
-                                    <div class="book-details">
-                                       <a href="../singlepage/singlepage.php?book_id=<?php echo $user['bookid']; ?>">
-                                        <span class="book-name"><?php echo $user['book_name']; ?></span>
-                    </a>
-                                    </div>
-                                </li>
-                            </ul>
-                        </div>
-                    <?php
-endif;?>
-
-        <?php endforeach;?>
+        <?php endforeach; ?>
     </div>
+
     <script src="../../JS/Background/background.js"></script>
-     <script>
-        function toggleBookDetails(userId) {
+    <script>
+          function toggleBookDetails(userId) {
             var bookDetails = document.getElementById('book-details-' + userId);
             if (bookDetails.style.display === 'none' || bookDetails.style.display === '') {
                 bookDetails.style.display = 'block';
@@ -135,8 +123,6 @@ endif;?>
                 bookDetails.style.display = 'none';
             }
         }
-
     </script>
-
 </body>
 </html>
