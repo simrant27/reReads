5792bf7d39992b4bd57fab93b65b14a3ed88905b
Asmitapaudel <asmeetapdl@gmail.com>
2023-10-09 23:07:06 +0545

    change on footer

--- HTML/footer/footer.php
+++ HTML/footer/footer.php
@@ -16,7 +16,68 @@
  <section id="policies" class="section">
         <div class="container">
             <h1>Policies</h1>
-            <p>Welcome to our second-hand books website. </p>
+            <h2>
+    Privacy Policy for reReads 
+    
+</h2>
+<p>
+    At reReads, accessible from rereads.com, one of our main priorities is the privacy of our visitors. This Privacy Policy document contains types of information that is collected and recorded by reReads and how we use it. <br> <br>
+
+    If you have additional questions or require more information about our Privacy Policy, do not hesitate to contact us. <br> <br>
+
+    This Privacy Policy applies only to our online activities and is valid for visitors to our website with regards to the information that they shared and/or collect on reReads. This policy is not applicable to any information collected offline or via channels other than this website. Our Privacy Policy was created with the help of the Free Privacy Policy Generator. <br> <br>
+</p>
+
+<h3>
+    Consent
+
+</h3>
+<p>
+    By using our website, you hereby consent to our Privacy Policy and agree to its terms. <br> <br>
+</p>
+<h4>
+    Information we collect
+</h4>
+<p>
+    The personal information that you are asked to provide, and the reasons why you are asked to provide it, will be made clear to you at the point we ask you to provide your personal information. <br> <br>
+
+    If you contact us directly, we may receive additional information about you such as your name, email address, phone number, the contents of the message and/or attachments you may send us, and any other information you may choose to provide. <br> <br>
+
+    When you register for an Account, we may ask for your contact information, including items such as name, company name, address, email address, and telephone number. <br> <br>
+</p>
+
+<h3>
+    How we use your information
+</h3>
+
+<p>
+    We use the information we collect in various ways, including to:
+        <p>Improve, personalize, and expand our website</p>
+        <p>
+            Understand and analyze how you use our website
+
+        </p>
+        <p>
+            Develop new products, services, features, and functionality
+
+        </p>
+        <p>
+            Communicate with you, either directly or through one of our partners, including for customer service, to provide you with updates and other information relating to the website, and for marketing and promotional purposes
+
+        </p>
+        <p>
+            Send you emails
+
+        </p>
+
+        <p>
+            Find and prevent fraud
+
+        </p>
+</p>
+
+
+            
         </div>
     </section>
 
@@ -26,9 +87,28 @@
             <h1>About Us</h1>
             <p>Welcome to our second-hand books website. We are passionate about providing high-quality used books to readers around the world. Our mission is to promote sustainability, literacy, and the joy of reading.</p>
             <p>Founded in 2023, our team of book enthusiasts scours the world for unique and well-preserved second-hand books. We believe in the power of stories to inspire and educate, and we want to make these stories accessible to everyone.</p>
-            <p>Quality is our top priority. Each book in our collection undergoes a rigorous inspection process to ensure it meets our standards. Whether you're a student looking for affordable textbooks or a collector in search of rare editions, you'll find something special in our catalog.</p>
+            <p> Whether you're a student looking for an affordable novel, you will find it at an affordable price.</p>
             <p>Thank you for joining us on our literary journey. We invite you to explore our website, browse our collection, and embark on new reading adventures with us.</p>
         </div>
+      <center>
+          <h3>Our Members</h3>
+          <span class="images">
+        <figure>
+            <img src="../../assets/admin/Asmita.jpg" alt="" >
+            <figcaption>Asmita Paudel</figcaption>
+        </figure>
+            <figure>
+                <img src="../../assets/admin/simran.jpg" alt="">
+                <figcaption>Simran Tamang</figcaption>
+
+            </figure>
+            <figure>
+
+                <img src="../../assets/admin/anmol.jpg" alt="">
+                <figcaption>Anmol Regmi</figcaption>
+            </figure>
+    </span>
+      </center>
     </section>
 
     <!-- Contact Us Section -->
